b21d61626332b6e0ab67bec9b55fd3eac26010d5
Eli Andrew <bracingflux@gmail.com>
2018-06-30 11:01:54 -0600

    added changes

--- public/a4.js
+++ public/a4.js
@@ -1,16 +1,10 @@
-function test() {
-	alert(window.location.href);	
-}
-
 $(document).ready(function (){
     $.ajax({                   
     	url: "/getProductNames",
     	data: { get_param: 'value' }, 
     	dataType: 'json',                   
-      // url: "/getProduct?id=2",              
   		type: "get",
   		success: function (data) { 
-  			// alert("here");
   			var $dropdown = $("#products");
         $.each(data, function(index, element) {
         	$dropdown.append($("<option />").text(this.name).val(this.id));
@@ -23,32 +17,21 @@ $(document).ready(function (){
        alert('request failed: ' + errorThrown);
        }           
    });
-});
 
-
-$('#products').on('change', function() {
-  alert("changed");
-})
-
-$("#products").change(function(){
-	alert("The text has been changed.");
-	var id = ("#products").val();
-	alert(id);
-	$.ajax({
-    	// url: "/getProductNames",
-    	// data: { get_param: 'value' }, 
-    	// dataType: 'json',                   
+   $('#products').on('change', function() {
+	var id = $("#products").val();
+	$.ajax({    	                 
       	url: "/getProduct?id=" + id,              
   		type: "get",
       success: function (res) {
-      	alert("success!");
           $('#product').text("Name: " + res.name + "\nPrice: " + res.price + "\nDescription: " + res.description);
         },
         complete: function () {
-        	alert("complete");
+        	// alert("complete");
         },
         fail: function(xhr, textStatus, errorThrown){
        alert('request failed: ' + errorThrown);
        }           
    });
+}) 
 });
