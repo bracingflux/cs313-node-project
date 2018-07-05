ae3d694110afc60e44f2c864f224da19195fbd62
Eli Andrew <bracingflux@gmail.com>
2018-07-05 16:47:08 -0600

    more changes

---
+++ public/js/a4.js
@@ -0,0 +1,66 @@
+$(document).ready(function (){
+    $.ajax({                   
+    	url: "/getProductNames",
+    	data: { get_param: 'value' }, 
+    	dataType: 'json',                   
+  		type: "get",
+  		success: function (data) { 
+  			var $dropdown = $("#products");
+        $.each(data, function(index, element) {
+        	$dropdown.append($("<option />").text(this.name).val(this.id));
+        });
+    },     
+        complete: function () {
+        	// alert("complete");
+        },
+        fail: function(xhr, textStatus, errorThrown){
+       alert('request failed: ' + errorThrown);
+       }           
+   });
+
+   $('#products').on('change', function() {
+	var id = $("#products").val();
+	$.ajax({    	                 
+      	url: "/getProduct?id=" + id,              
+  		type: "get",
+      success: function (res) {
+          $('#product').text("Name: " + res.name + "\nPrice: $" + res.price + "\nDescription: " + res.description);
+        },
+        complete: function () {
+        	// alert("complete");
+        },
+        fail: function(xhr, textStatus, errorThrown){
+       alert('request failed: ' + errorThrown);
+       }           
+   });
+})
+
+$('#productName').change(function() {
+ var name = $("#productName").val();
+ console.log(name);
+ $.ajax({                       
+     url: "http://api.walmartlabs.com/v1/search?apiKey=nsgjenyj5zedvuz746ugac4k&lsPublisherId=eliandrew&query=" + name,     /*http://api.walmartlabs.com/v1/search?apiKey=nsgjenyj5zedvuz746ugac4k&lsPublisherId=eliandrew&query=*/         
+     type: "get",
+      success: function (res) {
+        console.log(res);
+        var names = "";
+        var $target = $("body").find('#productInfo');
+        // $("#productInfo").empty(); // empty previous search results
+        
+        /*for (var i = 0; i < res.Search.length; i++) {
+          console.log(res.Search[i].Title);
+          titles = titles + "<span>" + res.Search[i].Title + "</span>" + "<button class='details' id='" + res.Search[i].imdbID + "'>Details</button><br>";
+        }*/
+        titles = "<p>" + res.items[0].name + "</p>";
+        $target.append(titles); 
+
+        },
+        complete: function () {
+           // alert("complete");
+        },
+        fail: function(xhr, textStatus, errorThrown){
+       alert('request failed: ' + errorThrown);
+       }           
+   });
+}) 
+});
